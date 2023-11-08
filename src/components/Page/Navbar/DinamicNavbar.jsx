21c78b9160490a2442dbda60150c26696bf7e631
sisthema-admin <149604398+sisthema-admin@users.noreply.github.com>
2023-11-08 13:15:12 +0100

    feat: fixed bug in navbar

--- src/components/Page/Navbar/DinamicNavbar.jsx
+++ src/components/Page/Navbar/DinamicNavbar.jsx
@@ -3,18 +3,18 @@ import { useState } from "react";
 import IconMenu from "@/components/ui/svg/IconMenu";
 import { PATH_ROUTE } from "@/util/const";
 import Link from "next/link";
+import useClickOut from "@/hooks/useClickOut";
 
 export default function DynamicNavbar() {
-    const [isActivate, setIsActivate] = useState(false);
-    const handleActivate = () => setIsActivate(prev => !prev);
-
+    const [ref, isActivate, setIsActivate] = useClickOut()
+    
     return (
         <div className="relative sm:hidden">
-            <span className="cursor-pointer" onClick={handleActivate}>
+            <span className="cursor-pointer" onClick={() => setIsActivate(prev => !prev)}>
                 <IconMenu size="30" className="block sm:hidden" isActivate={isActivate} />
             </span>
             {isActivate && (
-                <ul className="flex flex-col gap-2 w-48 sm:w-64 transition-all z-50 shadow-lg bg-blue-600 text-white rounded-lg py-5 absolute top-12 right-4">
+                <ul ref={ref} className="flex flex-col gap-2 w-48 sm:w-64 transition-all z-50 shadow-lg bg-blue-600 text-white rounded-lg py-5 absolute top-12 right-4">
                     {PATH_ROUTE.map(({ title, path }, key) => (
                         <li
                             key={key}
